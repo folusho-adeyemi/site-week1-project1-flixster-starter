caf62b7f71dafe38b8861de844878798fa46f751
Folusho Adeyemi <folushoadeyemiv@meta.com>
2023-06-15 00:06:08 -0700

    deleted previous comments for readability

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 console.log("Hello");
 
 
-
-
 // Constants
 const API_KEY = '291cfe5e18caa2f0caa8aa14be45de48';
 const API_URL = `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`;
@@ -71,59 +69,3 @@ async function loadMoreMovies() {
 searchForm.addEventListener('submit', handleFormSubmit);
 loadMoreBtn.addEventListener('click', loadMoreMovies);
 
-// const apiKey = '291cfe5e18caa2f0caa8aa14be45de48';
-// const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}`;
-// let pageNumber = 1;
-
-// function fetchMovies(page) {
-//     const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&page=${page}`;
-//     console.log(url);
-//     fetch(url)
-//         .then(response => response.json())
-//         .then(data => displayMovies(data.results))
-//         .catch(error => console.error(error));
-// }
-// function displayMovies(movies) {
-//     const gifsDiv = document.getElementById('gifsDiv');
-
-//     movies.forEach(movie => {
-//         const movieDiv = document.createElement('div');
-//         movieDiv.classList.add('movie');
-
-//         const title = document.createElement('h3');
-//         title.textContent = movie.title;
-
-//         const poster = document.createElement('img');
-//         poster.src = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
-//         poster.alt = movie.title;
-
-//         const votes = document.createElement('p');
-//         votes.textContent = `Votes: ${movie.vote_count}`;
-
-//         movieDiv.appendChild(title);
-//         movieDiv.appendChild(poster);
-//         movieDiv.appendChild(votes);
-
-//         gifsDiv.appendChild(movieDiv);
-//     });
-// }
-// function loadMoreMovies() {
-//     pageNumber++;
-//     fetchMovies(pageNumber);
-// }
-
-// document.getElementById('searchInout').addEventListener('submit', function(event) {
-//     event.preventDefault();
-//     const searchInput = document.getElementById('searchInput');
-//     const searchQuery = searchInput.value;
-    
-//     // Perform your search API call here with the searchQuery
-    
-//     // Clear the search input value
-//     searchInput.value = '';
-// });
-
-// document.getElementById('loadMoreBtn').addEventListener('click', loadMoreMovies);
-
-// // Fetch the initial movies on page load
-// fetchMovies(pageNumber);
